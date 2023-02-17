17aafbf99a48b0c4755ce81d0d7e69694ab96946
Ayat-Ali16 <103026846+Ayat-Ali16@users.noreply.github.com>
2023-02-17 23:28:23 +0200

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,22 +9,22 @@
  */
 int main(void)
 {
-0-positive_or_negative.c int n, l;
+int n, l;
 
-0-positive_or_negative.c srand(time(0));
-0-positive_or_negative.c n = rand() - RAND_MAX / 2;
-0-positive_or_negative.c l = n % 10;
-0-positive_or_negative.c if (l > 5)
-0-positive_or_negative.c {
-0-positive_or_negative.c 0-positive_or_negative.c printf("Last digit of %d is %d and is greater than 5\n", n, l);
-0-positive_or_negative.c }
-0-positive_or_negative.c else if (l == 0)
-0-positive_or_negative.c {
-0-positive_or_negative.c 0-positive_or_negative.c printf("Last digit of %d is %d and is 0\n", n, l);
-0-positive_or_negative.c }
-0-positive_or_negative.c else
-0-positive_or_negative.c {
-0-positive_or_negative.c 0-positive_or_negative.c printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
-0-positive_or_negative.c }
-0-positive_or_negative.c return (0);
+ srand(time(0));
+ n = rand() - RAND_MAX / 2;
+ l = n % 10;
+ if (l > 5)
+ {
+  printf("Last digit of %d is %d and is greater than 5\n", n, l);
+ }
+else if (l == 0)
+ {
+ printf("Last digit of %d is %d and is 0\n", n, l);
+ }
+else
+ {
+ printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+}
+return (0);
 }
